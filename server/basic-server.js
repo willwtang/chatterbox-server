2242827797e7276031f588e216336d416d70073c
willwtang <willwyatttang@gmail.com>
2016-05-09 15:16:13 -0700

    set up basic server

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,5 +1,15 @@
 /* Import node's http module: */
+
+var handleRequest = require('./request-handler');
+console.log(handleRequest);
+
 var http = require('http');
+var port = 3000;
+var ip = '127.0.0.1';
+
+var server = http.createServer(handleRequest);
+console.log('Listening on http://' + ip + ':' + port);
+server.listen(port, ip);
 
 
 // Every server needs to listen on a port with a unique number. The
@@ -7,12 +17,10 @@ var http = require('http');
 // normally already claimed by another server and/or not accessible
 // so we'll use a standard testing port like 3000, other common development
 // ports are 8080 and 1337.
-var port = 3000;
 
 // For now, since you're running this server on your local machine,
 // we'll have it listen on the IP address 127.0.0.1, which is a
 // special address that always refers to localhost.
-var ip = '127.0.0.1';
 
 
 
@@ -22,9 +30,6 @@ var ip = '127.0.0.1';
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
 
 // To start this server, run:
 //
@@ -38,4 +43,3 @@ server.listen(port, ip);
 // server.listen() will continue running as long as there is the
 // possibility of serving more requests. To stop your server, hit
 // Ctrl-C on the command line.
-
--- server/request-handler.js
+++ server/request-handler.js
@@ -13,6 +13,24 @@ this file and include it in basic-server.js so that it actually works.
 **************************************************************/
 
 var requestHandler = function(request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+
+  headers['Content-Type'] = 'text/plain';
+  response.writeHead(statusCode, headers);
+  response.end('Hello, ' + request.url.slice(1));
+};
+
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+console.log('request-handler.js', requestHandler);
+module.exports = requestHandler;
+
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,23 +45,18 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   // The outgoing status.
-  var statusCode = 200;
 
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,8 +65,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
-};
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -64,10 +75,3 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
-
